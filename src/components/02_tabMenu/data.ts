c6aad3eb03f6e53d9269498b33cfcdf1b2fe6c84
YERIM-BAK <rumiobu03@gmail.com>
2025-01-05 10:24:26 +0900

    탭메뉴 실습

---
+++ src/components/02_tabMenu/data.ts
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    "id": "6778da36dfe6ea6ff73c09e0",
+    "title": "Incididunt sunt velit irure Lorem reprehenderit deserunt proident tempor laborum labore do esse eu amet. Non consequat anim sunt aliqua.",
+    "description": "Dolor voluptate id consectetur deserunt irure minim laboris in reprehenderit exercitation. Nisi non id dolor eu do. Est esse aliqua Lorem laboris. Nostrud nulla magna voluptate ullamco culpa consequat consectetur ipsum eu nulla. Quis duis sint officia anim. In officia excepteur fugiat Lorem commodo ipsum aliquip exercitation eiusmod reprehenderit deserunt ea incididunt aliqua. Irure sunt quis labore incididunt veniam sint culpa consequat qui.\r\nIpsum nulla veniam officia id qui minim tempor elit duis reprehenderit excepteur proident elit deserunt. Consectetur dolore tempor incididunt cupidatat nisi do elit mollit Lorem aliqua duis ullamco aute do. Consectetur et excepteur tempor sit do quis.\r\nEst aute aliquip eu est ut magna ex est enim. Ea voluptate consectetur ullamco consequat occaecat officia voluptate Lorem reprehenderit sit culpa. Est nostrud officia labore aute irure eiusmod voluptate officia exercitation aute do minim aliquip sit. Culpa et laborum sint occaecat eiusmod qui nulla esse incididunt.\r\n"
+  },
+  {
+    "id": "6778da3661e50ecec5a6ec4b",
+    "title": "Commodo tempor sunt deserunt ea esse ullamco ex in pariatur.",
+    "description": "Tempor dolore voluptate voluptate qui voluptate laboris et. Ea do pariatur officia culpa et incididunt et aute. Irure esse quis velit adipisicing veniam. Eu est Lorem elit nostrud aliqua est consectetur.\r\nCommodo nulla minim occaecat ea anim nulla sit sint labore ea nulla in id velit. Consectetur ipsum id ut officia voluptate consequat. Culpa consequat eiusmod consectetur aute incididunt mollit. Commodo exercitation veniam sunt tempor fugiat. Id commodo excepteur ipsum velit aute. Aliquip commodo ipsum in ad occaecat laborum pariatur exercitation magna est magna enim. Pariatur sit Lorem ad amet elit est ullamco ullamco.\r\n"
+  },
+  {
+    "id": "6778da367f0d84afb3b33087",
+    "title": "In sunt ut id ullamco cupidatat officia non reprehenderit.",
+    "description": "Esse ipsum ea sit incididunt officia velit enim culpa culpa dolor mollit ullamco incididunt. Officia cupidatat nostrud voluptate nisi laboris laboris voluptate pariatur in occaecat non aute sunt. Non et qui culpa nisi.\r\nLorem exercitation voluptate deserunt elit. Deserunt enim ex est occaecat Lorem voluptate quis sint occaecat et ut nisi. Ut occaecat mollit consequat minim id quis quis laborum qui Lorem esse pariatur adipisicing. Sint labore officia laboris do commodo anim laborum cillum culpa non anim laborum laboris. Adipisicing veniam aliqua proident cupidatat excepteur ea id deserunt. Ex ipsum cillum cillum non.\r\n"
+  },
+  {
+    "id": "6778da3609cfb5810db476fe",
+    "title": "Tempor anim deserunt labore aliqua nulla occaecat. Eu ad adipisicing qui aliquip veniam anim in.",
+    "description": "Ad nostrud qui quis culpa excepteur. Ipsum nostrud irure culpa proident Lorem minim do. Occaecat consectetur id in id velit incididunt. Esse incididunt anim ad culpa ullamco minim consectetur. Magna pariatur elit cillum quis. Officia ipsum aute ipsum et nostrud culpa deserunt dolore ea.\r\nAdipisicing eiusmod adipisicing aute elit tempor nisi laboris. Aute anim laborum adipisicing aliquip amet id nostrud dolore non ut quis velit id nulla. Nisi irure eiusmod nisi veniam et exercitation commodo minim excepteur.\r\n"
+  },
+]
