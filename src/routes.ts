a283c6abf1ac583629e207defbc0bca04c45c632
YERIM-BAK <rumiobu03@gmail.com>
2025-01-06 09:34:47 +0900

    routes 툴팁까지 업로드

---
+++ src/routes.ts
@@ -0,0 +1,59 @@
+import Accordions from './components/01_accordion'
+import TabMenus from './components/02_tabMenu'
+import Tooltips from './components/03_tooltip'
+
+export const routePaths = [
+  '/',
+  '/accordion',
+  '/tabMenu',
+  '/tooltip',
+] as const
+export type ROUTE_PATH = (typeof routePaths)[number]
+
+type BaseRoute = {
+  key: ROUTE_PATH
+  link: ROUTE_PATH
+  name: string
+}
+export type ParentRoute = BaseRoute & {
+  children: ROUTE_PATH[]
+}
+export type ChildRoute = BaseRoute & {
+  children: ((props: unknown) => JSX.Element) | null
+}
+export type ROUTE = ParentRoute | ChildRoute
+
+export const routes: Record<ROUTE_PATH, ROUTE> = {
+  '/': {
+    key: '/',
+    link: '/',
+    name: 'root',
+    children: [
+      '/accordion',
+      '/tabMenu',
+      '/tooltip',
+    ],
+  },
+  '/accordion': {
+    key: '/accordion',
+    link: '/accordion',
+    name: '01. 아코디언',
+    children: Accordions,
+  },
+  '/tabMenu': {
+    key: '/tabMenu',
+    link: '/tabMenu',
+    name: '02. 탭메뉴',
+    children: TabMenus,
+  },
+  '/tooltip': {
+    key: '/tooltip',
+    link: '/tooltip',
+    name: '03. 툴팁',
+    children: Tooltips,
+  },
+}
+
+export const isParentRoute = (route: ROUTE): route is ParentRoute => Array.isArray(route.children)
+
+export const gnbRootList = (routes['/'] as ParentRoute).children.map(r => routes[r])
