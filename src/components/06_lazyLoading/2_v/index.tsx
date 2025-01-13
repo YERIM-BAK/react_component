a1166d679cbea89fc041f3ab44c0bb35d1b07fa6
YERIM-BAK <rumiobu03@gmail.com>
2025-01-13 20:29:05 +0900

    lazyImageBuilder export 시킴

--- src/components/06_lazyLoading/2_v/index.tsx
+++ src/components/06_lazyLoading/2_v/index.tsx
@@ -4,7 +4,7 @@ import cx from "../cx"
 import lazyLoad from "./lazyLoad"
 
 
-const lazyImageBuilder = (src: string, width: number, height: number) => {
+export const lazyImageBuilder = (src: string, width: number, height: number) => {
   const $elem = document.createElement('img')
   $elem.classList.add(cx('lazy'))
   $elem.setAttribute('width', width + 'px')
